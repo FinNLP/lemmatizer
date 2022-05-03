a6bd393c598e72df2adba46bcd46676d297a5157
Dmitriy <ikeyten@gmail.com>
2022-05-03 21:39:12 +1000

    Added lemmatizer("constructor") to tests

--- test/test.ts
+++ test/test.ts
@@ -7,6 +7,7 @@ let correct = 0;
 let total = 0;
 
 console.log(lemmatizer("unconditionally"));
+console.log(lemmatizer("constructor")); // special check for a name that is in the prototype
 
 readFileSync(__dirname+"/cases.txt","utf8").split("\n").forEach((testCase)=>{
     const lemma = testCase.split(" ")[0];
